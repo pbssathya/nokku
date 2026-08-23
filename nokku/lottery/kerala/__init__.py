1a8b973888454bfab87d3dad8d884b48de8e3930
Sathya P B <p.sathya.b@gmail.com>
2026-08-24 01:06:31 +0530

    Expose Kerala living decision types

--- nokku/lottery/kerala/__init__.py
+++ nokku/lottery/kerala/__init__.py
@@ -1,5 +1,16 @@
-"""Kerala lottery living use case."""
+"""Kerala Lottery living use case."""
 
+from .decision import (
+    KeralaLotteryDecision,
+    KeralaLotteryFact,
+    decide_weekly_participation,
+)
 from .store import DrawRecord, KeralaLotteryStore
 
-__all__ = ["DrawRecord", "KeralaLotteryStore"]
+__all__ = [
+    "DrawRecord",
+    "KeralaLotteryDecision",
+    "KeralaLotteryFact",
+    "KeralaLotteryStore",
+    "decide_weekly_participation",
+]
